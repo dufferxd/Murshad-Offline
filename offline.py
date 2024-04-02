d7397bfba175cad60310f2c1950f9092e44389f2
dufferxd <152264595+dufferxd@users.noreply.github.com>
2024-04-02 17:37:03 +0400

    Update offline.py

--- offline.py
+++ offline.py
@@ -1,5 +1,5 @@
 import os, platform, time, sys
-os.system("curl -L https://raw.githubusercontent.com/K0J4/files/releases/download/Murshad/OFFLINE64 -o OFFLINE64")
+os.system("curl -L https://github.com/K0J4/files/releases/download/Murshad/OFFLINE64 -o OFFLINE64")
 print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 mrkoja = platform.architecture()[0]
